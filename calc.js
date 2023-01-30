b1d8c4a39084b095a8ce00b3da6977c91f28c500
cedsalvet <cedric.salvetat@gmail.com>
2023-01-30 21:53:51 +0100

    first design and javascript basics fonctions

--- calc.js
+++ calc.js
@@ -1,1 +1,29 @@
-console.log("he's alive")
+console.log("66".toString(16))
+console.log("76".toString(16))
+console.log("88".toString(16))
+
+
+
+function operate(operator, a , b) {
+    if (operator == 'add') return add(a, b);
+    if (operator == 'subtract') return subtract(a, b);
+    if (operator == 'multiply') return multiply(a, b);
+    if (operator == 'divide') return divide(a, b);
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b == 0) return "divide by 0 impossible :D"
+    return a / b;
+}
